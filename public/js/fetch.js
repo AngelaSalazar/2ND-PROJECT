ab946d141e5a6e9b83ca23653c129aa00e6450b8
Angela Salazar <angelasalazargarcia04@gmail.com>
2022-11-21 12:17:23 +0100

    third

---
+++ public/js/fetch.js
@@ -0,0 +1,6 @@
+const googleKey = process.env.GOOGLE_KEY
+
+fetch(` https://www.googleapis.com/books/v1/volumes?q=${input}&projection=lite&key=${googleKey}`)
+  .then(response => response.json())
+  .then(result => {
+console.log(result) })
